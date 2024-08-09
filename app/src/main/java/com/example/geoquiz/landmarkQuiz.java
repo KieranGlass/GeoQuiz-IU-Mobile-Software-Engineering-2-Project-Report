bdaa0031e1d2abe26e4f9ee7d148fec2935751a2
kieran <kieranglass23@gmail.com>
2024-08-09 16:22:45 +0200

    Fleshing out of flag quiz activity and home screen

--- app/src/main/java/com/example/geoquiz/landmarkQuiz.java
+++ app/src/main/java/com/example/geoquiz/landmarkQuiz.java
@@ -1,44 +1,24 @@
 package com.example.geoquiz;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import androidx.appcompat.widget.Toolbar;
-public class MainActivity extends AppCompatActivity {
 
-
-    Button startBtn;
+public class landmarkQuiz extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_landmark_quiz);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        startBtn = findViewById(R.id.startBtn);
-        startBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-            startClick(startBtn);
-        }
-    });
-
     }
-    public void startClick(View view) {
-
-                Intent intent = new Intent(MainActivity.this, QuizDashboard.class);
-                startActivity(intent);
-
-        }
 }
